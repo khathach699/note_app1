c953c8b26fd602abf7aed565518bbb1687123b05
KhaThach <khathach699@gmail.com>
2025-05-12 23:43:47 +0700

    feat: add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:note_app1/app.dart';
+import 'package:note_app1/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
